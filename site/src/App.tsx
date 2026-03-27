a1e4459c4979a496d072e0a0b6ddb2ad6a751126
Brian Bales <48467575+brianbales99@users.noreply.github.com>
2026-03-26 20:50:15 -0700

    scorllig changes

--- site/src/App.tsx
+++ site/src/App.tsx
@@ -1,4 +1,5 @@
 import { Navigate, Route, Routes } from "react-router-dom";
+import { ScrollToTop } from "./components/ScrollToTop";
 import { HomePage } from "./pages/HomePage";
 import { ProjectsPage } from "./pages/ProjectsPage";
 import { PublicationsPage } from "./pages/PublicationsPage";
@@ -6,12 +7,15 @@ import { AboutPage } from "./pages/AboutPage";
 
 export default function App() {
   return (
-    <Routes>
-      <Route path="/" element={<HomePage />} />
-      <Route path="/projects" element={<ProjectsPage />} />
-      <Route path="/publications" element={<PublicationsPage />} />
-      <Route path="/about" element={<AboutPage />} />
-      <Route path="*" element={<Navigate to="/" replace />} />
-    </Routes>
+    <>
+      <ScrollToTop />
+      <Routes>
+        <Route path="/" element={<HomePage />} />
+        <Route path="/projects" element={<ProjectsPage />} />
+        <Route path="/publications" element={<PublicationsPage />} />
+        <Route path="/about" element={<AboutPage />} />
+        <Route path="*" element={<Navigate to="/" replace />} />
+      </Routes>
+    </>
   );
 }
